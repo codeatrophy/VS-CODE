c33b74e69803348ac6e59fe94750e18470e9b109
mirorim.2@gmail.com <mirorim.2@gmail.com>
2018-10-13 21:25:18 -0400

    asasf

---
+++ Arrays and Strings/Problem4StringReplace.c
@@ -0,0 +1,17 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+// this a program to replace all blanks with '%20' in a string
+int main() {
+//every blank space between the words is replaced with %20.
+//there should be enough space in the array to shift the original characters over
+char str[] = {'M', 'r',' ','J','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' ',' ',' ', '\0'};
+
+// holds the const size of the string including null term char
+int strLen = sizeof(str) / sizeof(char);
+
+
+    
+}
