6ff027f3190263fa0b1feb9f93f6baae26511dee
me <mirorim2@gmail.com>
2018-10-23 09:15:07 -0400

    oof how do character transfers work in C

--- Arrays and Strings/Problem4StringReplace.c
+++ Arrays and Strings/Problem4StringReplace.c
@@ -4,82 +4,49 @@
 #include <stdbool.h>
 #include <string.h>
 // this a program to replace all blanks with '%20' in a string
-// shift counter + blankspace finder
-// This function returns the amount of shift to occur for the string based on the extra spaces provided
-int shiftCount(char *myArray, int iter) {
- int charStrLen = iter;
- int shift = 0;
- int index = 0;
- // Count increases only when there are non consecutive blankspaces!
- for(int i = 0; i < charStrLen; i++) {
-     printf("%c",myArray[i]);
- }
-
-while(index <= charStrLen) {
-
-
-if(myArray[index] && myArray[index+1] == ' ')
-    shift++;
-
-    index++;
-}
-
-
- return shift-2;
-}
 // return the shifted string with in between spaces converted to %20
 // Note each character in %20 recieves it's own space
-char * replaceString(char *myArray, int shift, int iter) {
+char * replaceString(char *myArray, int iter) {
+//TODO: Properly transfer characters from pointer array to local array using a loop
 
-int count = iter;
+char temp[iter];
 
-char tempArray[iter];
-
-char blank[iter];
-
-strncpy(tempArray,myArray,iter);
-
-for(int i = 0; i <= iter; i++) {
-    if(tempArray[i] == ' ') {
-        blank[i] = '%';
-        blank[i+1] = '2';
-        blank[i+2] = '0';
-    }
+for(size_t i = 0; i < iter; i++) {
+    temp[i] = myArray[i];
+    printf("%c",temp[i]);
 }
-printf("\nblank is: ");
-for(int i = 0; i < iter; i++) {
-    printf("%c", blank[i]);
+
+printf("\n")
+
+
+for(size_t i = 0; i < iter; i++)
+{
     myArray[i] = ' ';
 }
- 
-for(int i = 0; i < iter; i++) {
-    myArray[i] = blank[i];
-}
- 
- return myArray;
-}
-int main() {
-//                           %   2   0      
-char strArray[] = {'M', 'r',' ','J','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' ',' ',' ', '\0'};
-// 19 characters
-// holds the const size of the string including null term char
-int iter = 0;
 
+for(size_t i = 0; i < iter; i++)
+{
+    printf("%c",myArray[i]);
+}
+
+
+}
+int main() {         
+char strArray[] = {'M', 'r',' ','J','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' ','\0'};
+//char sArray[] = {'M', 'r','%','2','0','J','o','h','n','%','2','0','S','M','i','t','h'};
+
+// the transformed array should be like this
+// 17 characters
+int iter = 0;
 while(strArray[iter] != '\0'){
     printf("\nmy array is at: %c", strArray[iter]);
     iter++;
 }
 
 // this is without the null term character
-printf("\nthe size of charStrLen is : %d\n", iter);
+printf("\nthe length of the array is : %d\n", iter);
 
-int shift = 0;
-
-shift = shiftCount(strArray,iter);
-// this should be 6
-printf("\nThe string will be shifted by a count of: %d", shift);
-
-replaceString(strArray,shift,iter);
+replaceString(strArray,iter);
 
 printf("\nResult: ");
 for(int i = 0; i < iter; i++) {
