fa7d48164986a06be5d5c007d330d1a88d9b99b0
art <aten@udel.edu>
2019-01-14 08:51:14 -0500

    Almost done stack smashing detected.

--- Arrays and Strings/Problem4StringReplace.c
+++ Arrays and Strings/Problem4StringReplace.c
@@ -3,21 +3,39 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-// this a program to replace all blanks with '%20' in a string
-// return the shifted string with in between spaces converted to %20
-// Note each character in %20 recieves it's own space
+
 char * replaceString(char *myArray, int len) {
-//TODO: Properly transfer characters from pointer array to local array using a loop
 
  char new[17];
+ int repIndex = 0;
 
+ for(int i = 0; i < len; i++) {
+     new[i] = myArray[i];
+     myArray[i] = ' ';
+     printf("new is: %c\n",new[i]);
 
+ }
 
+ for(int i = 0; i < len; i++) {
+
+     if(new[i] != '\40') {
+        myArray[repIndex] = new[i]; 
+        repIndex++;
+     } else {
+         myArray[repIndex] = '%';
+         myArray[repIndex+1] = '2';
+         myArray[repIndex+2] = '0';
+         repIndex+=3;
+     }
+
+ }
 }
+
+
+
 int main() {         
 char strArray[] = {'M', 'r',' ','J','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' ','\0'};
 //char strArray[] = {'M', 'r','%','2','0','J','o','h','n','%','2','0','S','M','i','t','h'};
-
 // the transformed array should be like this
 // 17 characters
 int len=0;
@@ -33,8 +51,9 @@ replaceString(strArray,len);
 
 printf("\nResult: ");
 for(int i = 0; i < len; i++) {
-    printf("\n:%c",strArray[i]);
+    printf("\%c",strArray[i]);
 }
 
+    printf("\n");
     return 0;
 }
