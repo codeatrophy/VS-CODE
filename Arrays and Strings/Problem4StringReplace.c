2b5950cb5b71e798ebb88dc72bc8f107f4c93f96
art <aten@udel.edu>
2019-01-14 22:26:32 -0500

    finished prob4

--- Arrays and Strings/Problem4StringReplace.c
+++ Arrays and Strings/Problem4StringReplace.c
@@ -1,12 +1,12 @@
-
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 
 char * replaceString(char *myArray, int len) {
 
- char new[17];
+ char new[len];
+ char transfer[len];
+
  int repIndex = 0;
 
  for(int i = 0; i < len; i++) {
@@ -17,28 +17,28 @@ char * replaceString(char *myArray, int len) {
  }
 
  for(int i = 0; i < len; i++) {
-
+     // NOTE: \40 is equivalent to blankspace, ' ' itself does not work.
      if(new[i] != '\40') {
-        myArray[repIndex] = new[i]; 
+        transfer[repIndex] = new[i]; 
         repIndex++;
      } else {
-         myArray[repIndex] = '%';
-         myArray[repIndex+1] = '2';
-         myArray[repIndex+2] = '0';
+         transfer[repIndex] = '%';
+         transfer[repIndex+1] = '2';
+         transfer[repIndex+2] = '0';
          repIndex+=3;
      }
-
  }
+
+ for(int i = 0; i < len; i++) 
+     myArray[i] = transfer[i];
 }
-
-
 
 int main() {         
 char strArray[] = {'M', 'r',' ','J','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' ','\0'};
 //char strArray[] = {'M', 'r','%','2','0','J','o','h','n','%','2','0','S','M','i','t','h'};
 // the transformed array should be like this
 // 17 characters
-int len=0;
+int len= sizeof(strArray) / sizeof(char);
 while(strArray[len] != '\0'){
     printf("\nmy array is at: %c", strArray[len]);
     len++;
@@ -47,11 +47,11 @@ while(strArray[len] != '\0'){
 // this is without the null term character
 printf("\nthe length of the array is : %d\n", len);
 
-replaceString(strArray,len);
+replaceString(strArray,len+1);
 
 printf("\nResult: ");
 for(int i = 0; i < len; i++) {
-    printf("\%c",strArray[i]);
+    printf("%c",strArray[i]);
 }
 
     printf("\n");
