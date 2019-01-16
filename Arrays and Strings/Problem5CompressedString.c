40c7c6db070ab4d920358aaec57224a917d9ff75
art <aten@udel.edu>
2019-01-15 19:01:36 -0500

    Almost done. Getting Segfault when copying to oroginal array.

--- Arrays and Strings/Problem5CompressedString.c
+++ Arrays and Strings/Problem5CompressedString.c
@@ -11,7 +11,6 @@ char * replaceString(char *myArray, int len) {
         int charCounter = 1; // since string len !=0, at least one word will be here.
         int counterIndex = 0; // tracks index for the letters
         int counterArray[len]; // stores the counts for the letters
-
         for(int i = 0; i < len; i++) {
             /* when the currentletter, and next letter are same, increment
                that letters counter, and the counter in the array
@@ -21,9 +20,8 @@ char * replaceString(char *myArray, int len) {
                 counterArray[counterIndex] = charCounter;
             } 
 
-            else if(myArray[i] != myArray[i+1] && myArray[i] != myArray[i-1]) {
+              else if(myArray[i] != myArray[i+1] && myArray[i] != myArray[i-1]) {
                 charCounter=1;
-                printf("\nwhat\n");
                 counterArray[counterIndex] = charCounter;
                 counterIndex++;
             }
@@ -31,13 +29,37 @@ char * replaceString(char *myArray, int len) {
             else if(myArray[i] != myArray[i+1]) {
                 counterArray[counterIndex++];
                 charCounter=1;
+
             }
         }
-
         for(int i = 0; i < 4; i++){
             printf("\n Stored in index %d of counterArray, this count of letters: %d",i, counterArray[i]);
         }
 
+        
+        int tempCounter = 0;
+        char compressedCharTemp[len]; //stores letters and numbers
+        int finalLength = 8;
+        int howFar = 0;
+        int counterJ = 0;
+        compressedCharTemp[0] = myArray[0];
+
+        for(int i = 1; i < 8; i++) {
+
+            if((i%2) == 0) {
+                howFar += counterArray[counterJ];
+                compressedCharTemp[i] = myArray[howFar];
+//printf("\nhow far = %d, compressedChar = %c, counterJ = %d\n", howFar, compressedCharTemp[i], counterJ);
+                counterJ++;
+            }else {
+                compressedCharTemp[i] = counterArray[tempCounter]+'0';
+                tempCounter++;
+            }
+        }   
+        printf("\ntempArray is: \n");
+        for(int i = 0; i < finalLength; i++){
+            printf("%c",compressedCharTemp[i]);
+        }
     }
 }
 
